5b4c3e4d6d69a4008447ba23ae2d14dd5c885855
LueptowK <kylelueptow@gmail.com>
2019-06-03 12:32:59 -0500

    fixes alwaysfail decorator

---
+++ Assets/Personal/Scripts/Enemy Scripts/Behavior Trees/DecoratorAlwaysFail.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BehaviorAlwaysFail : BehaviorNode
+{
+    public BehaviorAlwaysFail(BehaviorNode[] nodeChildren, Dictionary<string, object> nodeContext) : base(nodeChildren, nodeContext)
+    {
+        children = nodeChildren;
+        context = nodeContext;
+    }
+
+    public override void Update()
+    {
+        base.Update();
+    }
+
+    public override statusValues FixedUpdate()
+    {
+        return statusValues.failure;
+    }
+}
